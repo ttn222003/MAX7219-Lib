2ae5ef6c99406b8d1108e6e2823428f671eaca97
ttn222003 <truongtrongnhan.222003@gmail.com>
2024-03-29 23:53:29 +0700

    main

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 
 SPI_HandleTypeDef hspi1;
 
-uint8_t character[] = {
+/*uint8_t character[] = {
 	0x00, 0x07, 0x05, 0x05, 0x05, 0x05, 0x05, 0x07, // 0
 	0x00, 0x01, 0x03, 0x05, 0x01, 0x01, 0x01, 0x01, // 1
 	0x00, 0x07, 0x01, 0x01, 0x07, 0x04, 0x04, 0x07, // 2
@@ -18,15 +18,25 @@ uint8_t character[] = {
 
 uint8_t special_character[] = {
 	0x00, 0x66, 0x99, 0x81, 0x81, 0x42, 0x24, 0x18 // heart character
-};
+};*/
 
 int main(void)
 {
-    initMAX7219(NODEC, 15, DIS7);
+    //initMAX7219(NODEC, 15, DIS7);
+	initMAX7219(DECALL, 15, DIS7);
     while (1)
     {
-       displayACharacter(0, special_character);
-       // shiftingACharacter(5, character, 1);
+       /*displayACharacter(0, special_character);
+       // shiftingACharacter(5, character, 1);*/
+
+		displayNumber(235);
+	   	HAL_Delay(2000);
+		displayNumber(235.6);
+		HAL_Delay(2000);
+		displayNumber(-235);
+	   	HAL_Delay(2000);
+		displayNumber(-235.6);
+		HAL_Delay(2000);
     }
     
     return 0;
